3c2bf43b604418ffa582b41db3e775333e1e82d6
Guillaume Bauer <guillaume.bauer.418@gmail.com>
2015-04-09 16:48:10 +0200

    Initial Commit

---
+++ konami.js
@@ -0,0 +1,25 @@
+var Konami = {};
+
+Konami.table = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+Konami.I = 0;
+Konami.cooldown = {};
+Konami.onCodePerform = function(){};
+
+document.getElementsByTagName('body')[0].addEventListener('keydown', function(event) {
+	
+	if (event.keyCode == Konami.table[Konami.I]) {
+		if (Konami.cooldown) {
+			clearTimeout(Konami.cooldown);
+		}
+		Konami.cooldown = setTimeout(function() {
+			Konami.I = 0;
+		}, 1000);
+		if (Konami.I == 9) {
+			Konami.onCodePerform();
+		} else {
+			Konami.I++;
+		}
+	} else {
+		Konami.I = 0;
+	}
+});
